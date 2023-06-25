d24be93bdac1acfb13ac59ae5bb6e3086e527e46
mirisan <mirisan.ravindran@ontariotechu.net>
2023-06-24 23:03:41 -0400

    Added new Designs

--- src/Components/ListTitle.tsx
+++ src/Components/ListTitle.tsx
@@ -1,7 +1,6 @@
 export function ListTitle({ title, icon }: { title: string; icon?: string }) {
   return (
     <div className="list-title">
-      <img src={`/icons/${icon}.svg`} height={20} width={20} />
       <h2>{title}</h2>
     </div>
   );
